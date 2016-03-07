c717eb66a20c9e5f92f9e6c1a2ea7444aa05ba7a
Ilnur Nasyrov <ilnur.nasyrov.92@gmail.com>
2016-03-08 00:26:25 +0300

    Use file desriptors instead of IO objects

--- spec/lib/bignums_elector_spec.rb
+++ spec/lib/bignums_elector_spec.rb
@@ -3,18 +3,29 @@ require 'spec_helper'
 describe Nmax::BignumsElector do
   describe '.elect' do
     let(:bignums) { %w(11 4 22 6 333) }
-    let(:expected_bignums) { %w(11 22 333) }
-    let(:n) { 3 }
-    let(:fd) do
+    let(:size) { 3 }
+    let(:input) do
       rd, wr = IO.pipe
       wr.write(bignums.join("random delimiter"))
       wr.close
-      rd.to_i
+      rd
+    end
+
+    let(:output) do
+      rd, wr = IO.pipe
+      { rd: rd, wr: wr}
+    end
+
+    before do
+      described_class.elect(
+        size: size,
+        input_fd: input.to_i,
+        output_fd: output[:wr].to_i
+      )
     end
 
     it 'returns expected bignums' do
-      result = described_class.elect(n, fd)
-      expect(result).to eq expected_bignums
+      expect(output[:rd].read).to eq "11\n22\n333\n"
     end
   end
 end
